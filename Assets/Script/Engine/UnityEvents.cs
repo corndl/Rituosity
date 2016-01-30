a513e0094f4fc8ea2b07109d0fc99882c48d3498
Loïc Defrance <loic.defrance@gmail.com>
2016-01-30 02:33:31 +0100

    Serialized event classes. Added Vector3 and Quaternion

--- Assets/Script/Engine/UnityEvents.cs
+++ Assets/Script/Engine/UnityEvents.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine.Events;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Engine
 {
+    [SerializeField]
     public class BoolEvent : UnityEvent<bool> { }
+
+    [SerializeField]
     public class StringEvent : UnityEvent<string> { }
+
+    [SerializeField]
     public class FloatEvent : UnityEvent<float> { }
+
+    [SerializeField]
     public class IntEvent : UnityEvent<int> { }
+
+    [SerializeField]
+    public class Vector3Event : UnityEvent<Vector3> { }
+
+    [SerializeField]
+    public class QuaternionEvent : UnityEvent<Quaternion> { }
 }
