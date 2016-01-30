9b6f6f98b668535edc1c318a3710333caa4d2866
Loïc Defrance <loic.defrance@gmail.com>
2016-01-30 10:34:33 +0100

    City generation with block pulling (TODO mob generation)

---
+++ Assets/Script/Engine/ObjectPooler.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Engine
+{
+    public class ObjectPooler : MonoBehaviour
+    {
+        #region Properties
+        [SerializeField]
+        int m_PoolSize = 100;
+        #endregion
+
+        #region API
+        public int PoolSize
+        {
+            get
+            {
+                return m_PoolSize;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate all the objects we need to fill the pool.
+        /// </summary>
+        /// <param name="prefabs">The prefab to instantiate.</param>
+        public List<GameObject> GeneratePool(GameObject[] prefabs, int poolSize)
+        {
+            m_PoolSize = poolSize;
+
+            // GeneratePool should only be called once
+            if (m_Available.Count + m_InUse.Count == m_PoolSize)
+            {
+                return null;
+            }
+
+            // Instantiate m_poolSize GameObjects
+            for (int i = 0; i < m_PoolSize; i++)
+            {
+                Generate(prefabs[Random.Range(0, prefabs.Length)]);
+            }
+
+            return m_Available;
+        }
+
+        /// <summary>
+        /// Return an instance of the prefab.
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        public GameObject GetObject(GameObject[] prefabs)
+        {
+            // Returns first available element of the pool
+            // If there's no available object, create one
+            lock (m_Available)
+            {
+                // Check if there are objects available
+                if (m_Available.Count != 0)
+                {
+                    // Get first available element and place it in m_inUse
+                    GameObject obj = m_Available[0];
+                    m_InUse.Add(obj);
+                    m_Available.RemoveAt(0);
+
+                    return obj;
+                }
+
+                else
+                {
+                    // If there's no object available, generate one and add it 
+                    // to the pool
+                    GameObject obj = Generate(prefabs[Random.Range(0, prefabs.Length)]);
+                    m_InUse.Add(obj);
+                    m_Available.RemoveAt(0);
+                    m_PoolSize++;
+                    return obj;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Remove an object from inUse and put it in the available list.
+        /// </summary>
+        /// <param name="obj"></param>
+        public void ReleaseObject(GameObject obj)
+        {
+            if (m_InUse.Contains(obj))
+            {
+                lock (m_Available)
+                {
+                    m_Available.Add(obj);
+                    m_InUse.Remove(obj);
+                }
+            }
+
+        }
+
+        #endregion
+
+        #region Private
+        List<GameObject> m_Available = new List<GameObject>();
+        List<GameObject> m_InUse = new List<GameObject>();
+
+        private GameObject Generate(GameObject prefab)
+        {
+            GameObject obj = Instantiate(prefab);
+            m_Available.Add(obj);
+            return obj;
+        }
+        #endregion
+    }
+}
---
+++ Assets/Script/Procedural/CityGenerator.cs
@@ -0,0 +1,79 @@
+﻿using Engine;
+using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections.Generic;
+
+namespace Procedural
+{
+    public class CityGenerator : MonoBehaviour
+    {
+        #region Properties
+        [Header("Blocks")]
+        [SerializeField]
+        ObjectPooler m_BlockPool = null;
+        [SerializeField]
+        GameObject[] m_BlockPrefabs = new GameObject[0];
+        [SerializeField]
+        int m_GridSize = 5;
+        [SerializeField]
+        float m_BlockSize = 50f;
+        [SerializeField]
+        GameObject m_CityParent = null;
+
+        [Header("Mobs")]
+        [SerializeField]
+        ObjectPooler m_MobPool = null;
+        [SerializeField]
+        GameObject[] m_MobPrefabs = new GameObject[0];
+
+        [Header("Player")]
+        [SerializeField]
+        Motor m_Player = null;
+        #endregion
+
+        #region Unity
+        void Awake()
+        {
+            Assert.IsNotNull(m_BlockPool, "Block Pool is null");
+            Assert.IsNotNull(m_MobPool, "Mob Pool is null");
+
+            GenerateCity();
+        }
+
+        void Update()
+        {
+            // foreach block, if > distance max, release
+        }
+        #endregion
+
+        #region Private
+        GameObject[,] m_Tiles;
+
+        void GenerateCity()
+        {
+            List<GameObject> blocks = m_BlockPool.GeneratePool(m_BlockPrefabs, (int)Mathf.Pow(m_GridSize, 2));
+            m_Tiles = new GameObject[m_GridSize, m_GridSize];
+            int randRotation = 0;
+            GameObject block;
+            
+            
+            // Tile
+            for (int i = 0; i < m_GridSize; i++)
+            {
+                for (int j = 0; j < m_GridSize; j++)
+                {
+                    block = m_BlockPool.GetObject(m_BlockPrefabs);
+                    // Random block rotation
+                    randRotation = Random.Range(0, 4);
+                    block.transform.rotation = new Quaternion(0, randRotation * 90, 0, block.transform.rotation.w);                
+
+                    m_Tiles[i, j] = block;
+                    m_Tiles[i, j].transform.SetParent(m_CityParent.transform, false);
+                    m_Tiles[i, j].transform.position = new Vector3(i * m_BlockSize, 0, j * m_BlockSize);
+                }
+            }
+            
+        }
+        #endregion
+    }
+}
