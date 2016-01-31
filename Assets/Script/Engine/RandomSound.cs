7b057181786811e185337778312cf5d7816851e8
Loïc Defrance <loic.defrance@gmail.com>
2016-01-31 14:19:11 +0100

    Sound exit enter viscera randomize

---
+++ Assets/Script/Engine/RandomSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Engine
+{
+    public class RandomSound : MonoBehaviour
+    {
+        #region Properties
+        [SerializeField]
+        AudioSource[] m_AudioSources = new AudioSource[0];
+        #endregion
+
+        #region Unity
+        void Awake()
+        {
+            m_AudioSources = gameObject.GetComponentsInChildren<AudioSource>();
+        }
+        #endregion
+
+        #region API
+        public void PlayRandom()
+        {
+            m_AudioSources[(int)Random.Range(0, m_AudioSources.Length)].Play();
+        }
+        #endregion
+    }
+}
