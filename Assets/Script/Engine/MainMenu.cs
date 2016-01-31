53333fc639a23211bc187a9729d64b2ecf6f0e0e
Loïc Defrance <loic.defrance@gmail.com>
2016-01-31 14:53:53 +0100

    Ready for build

--- Assets/Script/Engine/MainMenu.cs
+++ Assets/Script/Engine/MainMenu.cs
@@ -6,6 +6,10 @@ namespace Engine
 {
     public class MainMenu : MonoBehaviour
     {
+        #region Events
+        public UnityEngine.Events.UnityEvent onLoad = new UnityEngine.Events.UnityEvent();
+        #endregion
+
         #region Unity
         void Awake()
         {
@@ -14,8 +18,10 @@ namespace Engine
 
         void Update()
         {
-            if((Input.touchSupported && Input.GetTouch(0).tapCount > 0) || Input.GetMouseButton(0) || Cardboard.SDK.Triggered) { 
+            if((Input.touchSupported && Input.GetTouch(0).tapCount > 0) || Input.GetMouseButton(0) || Cardboard.SDK.Triggered) 
+            { 
                 Debug.Log("Trigger Chargement Scene");
+                onLoad.Invoke();
                 SceneManager.LoadScene(1);
             }
         }
